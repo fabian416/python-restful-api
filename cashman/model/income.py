09b45b47847a401fbe78ebe14b3c0ecd13849727
fabian <fabian@MacBook-Air-de-fabian.local>
2023-06-21 03:30:35 -0300

    Initial commit: Uploading Flask API project

---
+++ cashman/model/income.py
@@ -0,0 +1,29 @@
+from marshmallow import post_load
+
+
+from .transaction import Transaction, TransactionSchema
+
+from .transaction_type import TransactionType
+
+
+
+
+class Income(Transaction):
+
+    def __init__(self, description, amount):
+        
+        super(Income, self).__init__(description, amount, TransactionType.INCOME)
+
+    def _repr__(self):
+    
+        return '<Income(name={self.description!r})>'.format(self=self)
+
+
+class IncomeSchema(TransactionSchema):
+
+    @post_load
+
+    def make_income(self, data, **kwargs):
+
+        return Income(**data)
+       
---
+++ cashman/model/transaction_type.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class TransactionType(Enum):
+
+    INCOME = "INCOME"
+
+    EXPENSE = "EXPENSE"
